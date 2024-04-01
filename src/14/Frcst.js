296563732689d557b6d4c1af3abe505e11f31ce3
baejimill <baejione@naver.com>
2024-04-01 17:56:13 +0900

    라우팅

--- src/14/Frcst.js
+++ src/14/Frcst.js
@@ -1,21 +1,35 @@
 import TailInput from "../UI/TailInput"
 import TailSelect from "../UI/TailSelect"
 import TailButton from "../UI/TailButton"
+import getxy  from "./getxy.json"
 import { useState, useEffect, useRef } from "react"
 
 export default function Frcst() {
+  
+  const ops = getxy.map(item=> item['1단계']);
+
+  const [x, setX] = useState();
+  const [y, setY] = useState();
+    
   const dRef = useRef();
   const sRef = useRef();
-  const ops = []
 
   const handleDate = ()=>{
     console.log(dRef.current.value)
   }
   const handleArea = ()=>{
+    let tm = getxy.filter(item => item['1단계'] === sRef.current.value)
+   
+    setX(tm[0]["격자 X"]);
+    setY(tm[0]["격자 Y"]);
+}  
+useEffect(()=>{
+  console.log(x,y)
+},[x,y])
+  
 
-  }
   return (
-    <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-2 p-2">
+    <div className="justify-start w-full grid grid-cols-1 md:grid-cols-2 gap-2 p-2">
       <div>
       <TailInput type = "date"
                   inputRef  = {dRef}
